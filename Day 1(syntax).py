05bb38cee2c88ae813a7ad4cd22d8672cc2f9b01
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2025-12-07 21:34:04 +0530

    Add files via upload

---
+++ Day 1(syntax).py
@@ -0,0 +1,91 @@
+'''name="Anirudh"#Q1
+age=18
+city="nellore"
+print(f"my name is {name} i am {age} years old and i live in {city}")'''
+#q2
+'''length=2
+breadth=3
+area=length*breadth
+sum=length+breadth
+perimetre=2*sum
+print(area)
+print(perimetre)'''
+#q3
+'''a=int(input("enter the number:-"))
+b=int(input("enter the number:-"))
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)'''
+#q4
+'''price=20
+price_tax=20+(0.18*price)
+print(price_tax)'''
+#q5
+'''
+num1=98
+num2=9.8
+name="anirudh"
+is_correct=True
+is_false=None
+print(type(num1))
+print(type(num2))
+print(type(name))
+print(type(is_correct))
+print(type(is_false))'''
+#q6
+'''
+pi= 3.14159
+pi=int(pi)
+print(pi)'''
+#q7
+'''
+num_str="123"
+num_int=int(num_str)
+m_n=num_int*2
+print(m_n)'''
+#q8
+'''radius=float(input("enter the radius:-"))
+pi= 3.14159
+area=pi*radius*radius
+print(area)
+'''
+#q9
+'''num1=17
+num2=5
+quo=num1//num2
+rem=num1%num2
+print(quo)
+print(rem)
+'''
+#q10
+'''principal=float(input("enter the principal:-"))
+rate=float(input("Enter The rate:-"))
+time=float(input("Enter THE TIME:-"))
+amount=principal*(1+rate/100)**time
+print(amount)'''
+#Q11
+'''num=int(input("Enter The Number"))
+if(10<=num<=20):
+    print("the num is b/w 10 and 20")
+else:
+    print("not b/w 10 and 20")'''
+#q12
+'''if num % 3 == 0 and num % 5 == 0:
+    print("Divisible by both 3 and 5")
+elif num % 3 == 0:
+    print("Divisible by 3")
+elif num % 5 == 0:
+    print("Divisible by 5")
+else:
+    print("Not divisible by 3 or 5")
+
+'''
+#Q14
+'''year=int(input("Enter The Year:-"))
+if(year%4!=0):
+    print("its leap year")
+else:
+    print("its a leap year")'''
+#q15
